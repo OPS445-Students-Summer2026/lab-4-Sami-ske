312d270146364e5210528c87dc73a0159a5860a4
Sadman Islam <sislam117@myseneca.ca>
2026-06-04 23:51:40 -0400

    Completed Lab 4 - all scripts passed

---
+++ lab4e.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# Author ID: [seneca_id]
+
+def is_digits(sobj):
+    # Loop through each character in sobj
+    for char in sobj:
+        # Checking if the character is NOT one of the digit characters
+        if char not in '0123456789':
+            return False
+    return True
+
+if __name__ == '__main__':
+    test_list = ['x3058','3058','8503x','8503']
+    for item in test_list:
+        if is_digits(item):
+            print(item,'is an integer.')
+        else:
+            print(item,'is not an integer.')
